3a6f3226575c881936e275c7171d570515f7973d
A cup <georgiyegoriy@gmail.com>
2025-04-25 13:16:39 +0300

    Update waybar.nix

--- packages/waybar.nix
+++ packages/waybar.nix
@@ -2,7 +2,10 @@
   environment.systemPackages = with pkgs; [
     waybar
   ];
-  xdg.configFile."waybar".source = ../.config/waybar;
+  xdg.configFile."waybar".source = builtins.path {
+    path = ../.config/waybar;
+    name = "waybar-config";
+  };
   #xdg.configFile."waybar/config.jsonc".source = ./.config/waybar/config.jsonc;
   #xdg.configFile."swaync/config.json".source = ./config.json;
 }
