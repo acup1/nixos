55332f6a254d5d7e2571b3891631af47ea386657
A cup <georgiyegoriy@gmail.com>
2025-04-25 11:54:55 +0300

    qwe

--- packages/waybar.nix
+++ packages/waybar.nix
@@ -2,7 +2,7 @@
   environment.systemPackages = with pkgs; [
     waybar
   ];
-  xdg.configFile."waybar".source = ./.config/waybar;
+  xdg.configFile."waybar".source = ../.config/waybar;
   #xdg.configFile."waybar/config.jsonc".source = ./.config/waybar/config.jsonc;
   #xdg.configFile."swaync/config.json".source = ./config.json;
 }
