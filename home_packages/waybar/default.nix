484db31f93a3b68932fa2672abfcb1b5729be59c
A cup <georgiyegoriy@gmail.com>
2025-04-30 14:13:26 +0300

    qwe

--- home_packages/waybar/default.nix
+++ home_packages/waybar/default.nix
@@ -3,9 +3,12 @@
   config,
   inputs,
   ...
-}: {
+}: let
+  repo = builtins.fetchGit {url = "https://github.com/Anik200/dotfiles";};
+in {
   home.packages = with pkgs; [
     waybar
   ];
-  xdg.configFile."waybar".source = ./.config/waybar;
+  #xdg.configFile."waybar".source = ./.config/waybar;
+  xdg.configFile."waybar".source = "${repo.outPath}/.config/waybar";
 }
