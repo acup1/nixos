301bbd6b48464eb8d3584157d4025430af846102
acup1 <georgiyegoriy@gmail.com>
2025-07-11 04:28:04 +0300

    qwe

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -91,8 +91,8 @@
   ];
   programs.system-config-printer.enable = true;
 
-  # Enable sound with pipewire.
   programs.zsh.enable = true;
+  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -100,12 +100,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    jack.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -156,6 +151,7 @@
       libgbm
     ];
   };
+
   #environment.systemPackages = with unstable-pkgs; [
   #  amnezia-vpn
   #];
--- home_packages/zsh/default.nix
+++ home_packages/zsh/default.nix
@@ -6,5 +6,7 @@
 }: {
   home.file = {
     ".zshrc".source = ./.zshrc;
+    ".oh-my-zsh".source = "${pkgs.oh-my-zsh}/share/oh-my-zsh";
+    ".oh-my-zsh/themes/acup.zsh-theme".source = ./acup.zsh-theme;
   };
 }
