5a59e7656052f8c2a39830ee7a06dee1b23b6216
acup1 <georgiyegoriy@gmail.com>
2025-07-11 06:05:12 +0300

    starship

--- home_packages/zsh/default.nix
+++ home_packages/zsh/default.nix
@@ -4,9 +4,11 @@
   inputs,
   ...
 }: {
+  home.packages = with pkgs; [
+    starship
+  ];
+
   home.file = {
     ".zshrc".source = ./.zshrc;
-    ".oh-my-zsh".source = "${pkgs.oh-my-zsh}/share/oh-my-zsh";
-    #".oh-my-zsh/themes/acup.zsh-theme".source = ./acup.zsh-theme;
   };
 }
