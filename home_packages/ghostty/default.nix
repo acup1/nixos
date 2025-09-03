b0af1b71dad6500bbf0cfe7b9b5baac15e4351e8
acup1 <georgiyegoriy@gmail.com>
2025-09-03 14:52:03 +0300

    idk

--- home_packages/ghostty/default.nix
+++ home_packages/ghostty/default.nix
@@ -1,12 +1,10 @@
-{
-  pkgs,
-  config,
-  inputs,
-  ...
+{ pkgs
+, config
+, inputs
+, ...
 }: {
   home.packages = with pkgs; [
     ghostty
   ];
-  #xdg.configFile."waybar".source = ./.config/waybar;
   xdg.configFile."ghostty".source = ./.config/ghostty;
 }
--- home_packages/hypr/default.nix
+++ home_packages/hypr/default.nix
@@ -1,8 +1,10 @@
-{
-  pkgs,
-  config,
-  inputs,
-  ...
+{ pkgs
+, config
+, inputs
+, ...
 }: {
+  home.packages = with pkgs; [
+    swaybg
+  ];
   xdg.configFile."hypr".source = ./.config/hypr;
 }
