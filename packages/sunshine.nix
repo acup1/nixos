b325cd42eb86909fc252907516c2e30919d33af4
A cup <georgiyegoriy@gmail.com>
2026-09-14 00:53:30 +0300

    sunshine csrf fix

--- packages/sunshine.nix
+++ packages/sunshine.nix
@@ -6,5 +6,8 @@
 
     # Универсальный захват Wayland через DRM/KMS.
     capSysAdmin = true;
+
+    settings.csrf_allowed_origins =
+      "https://nixos:47990,https://nixos.local:47990,https://192.168.0.2:47990,https://ru.cupscloud.ru:47990";
   };
 }
