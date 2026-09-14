16fddb9061830a54461358c06453e9798aba3e36
A cup <georgiyegoriy@gmail.com>
2026-09-14 23:51:48 +0300

    sunshine-frp

---
+++ configuration/sunshine-frp.nix
@@ -0,0 +1,45 @@
+{ ... }: {
+  services.frp.instances.sunshine = {
+    enable = true;
+    role = "client";
+    # Не хранить токен в Git. Файл: FRP_TOKEN=<одинаковый токен с VPS>, mode 0600.
+    environmentFiles = ["/etc/frp-sunshine.env"];
+
+    settings = {
+      serverAddr = "ru.cupscloud.ru";
+      serverPort = 7000;
+      loginFailExit = false;
+      udpPacketSize = 1500;
+
+      auth = {
+        method = "token";
+        token = "{{ .Envs.FRP_TOKEN }}";
+      };
+
+      transport = {
+        protocol = "quic";
+        poolCount = 5;
+        tls.enable = true;
+      };
+
+      log = {
+        to = "console";
+        level = "info";
+      };
+
+      proxies = map (port: {
+        name = "sunshine-udp-${toString port}";
+        type = "udp";
+        localIP = "127.0.0.1";
+        localPort = port;
+        remotePort = port;
+      }) [
+        47998
+        47999
+        48000
+        48002
+        48010
+      ];
+    };
+  };
+}
